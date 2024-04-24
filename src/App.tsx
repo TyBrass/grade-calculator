59a57c716a0ea1609a3158a495f1b51c37c6b533
Ty Brass <tybrass@outlook.com>
2024-04-23 21:09:10 -0500

    Restructure into lists

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,32 @@ function App() {
         <p className='my-[10px] max-w-[600px]'>Input weighting and received grades for each piece of coursework to see what average is required on remaining coursework to achieve certain final marks in your class</p>
         <div className='bg-slate-200 w-[400px]'>
             <div className='flex justify-evenly'>
-                <h2>Weights</h2>
-                <h2>Grades</h2>
-            </div>
-            {grades.map(c => (
-                <>
-                <div className='flex justify-evenly mb-[8px]'>
-                    <input className='w-[50px]' type="number" min='0' max='100' placeholder='0' />
-                    <input className='w-[50px]' type="number" min='0' max='100' placeholder='0' />
+                <div className='flex flex-col justify-evenly mb-[4px]'>
+                    <h2>Weights</h2>
+                    <ul>
+                        {grades.map(c => (
+                            <>
+                            <li className='flex justify-evenly mb-[8px]'>
+                                <input className='w-[50px]' type="number" min='0' max='100' placeholder='0' />
+                            </li>
+                            </>
+                        ))}
+                    </ul>
                 </div>
-                </>
-            ))}
-            <div className='flex justify-evenly mb-[4px] border-t-2 border-white'>
+                <div className='flex flex-col justify-evenly mb-[4px]'>
+                    <h2>Grades</h2>
+                    <ul>
+                        {grades.map(c => (
+                            <>
+                            <li className='flex justify-evenly mb-[8px]'>
+                                <input className='w-[50px]' type="number" min='0' max='100' placeholder='0' />
+                            </li>
+                            </>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <div className='flex justify-evenly mb-[4px] border-t-4 border-white'>
                 <div>
                     <h2 className='italic'>Weight Remaining</h2>
                     {100 * grades.reduce((accumulator, currentValue) => accumulator + currentValue[0], 0)}
@@ -30,6 +44,40 @@ function App() {
                 <div>
                     <h2 className='italic'>Current Grade</h2>
                     {grades.reduce((accumulator, currentValue) => accumulator + currentValue[0] * currentValue[1], 0) * grades.reduce((accumulator, currentValue) => accumulator + currentValue[0], 0)}
+                </div>
+            </div>
+            <div className='flex justify-evenly mb-[4px] border-t-4 border-white'>
+                <div>
+                    <h2 className='italic'>To Get...</h2>
+                    <ul>
+                        <li><input className='w-[50px] text-center' type="number" min='0' max='100' placeholder='0' /></li>
+                        <li>95</li>
+                        <li>90</li>
+                        <li>85</li>
+                        <li>80</li>
+                        <li>75</li>
+                        <li>70</li>
+                        <li>65</li>
+                        <li>60</li>
+                        <li>55</li>
+                        <li>50</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2 className='italic'>You Need...</h2>
+                    <ul>
+                        <li><input className='w-[50px] text-center' type="number" min='0' max='100' placeholder='0' /></li>
+                        <li>{0}</li>
+                        <li>{0}</li>
+                        <li>{0}</li>
+                        <li>{0}</li>
+                        <li>{0}</li>
+                        <li>{0}</li>
+                        <li>{0}</li>
+                        <li>{0}</li>
+                        <li>{0}</li>
+                        <li>{0}</li>
+                    </ul>
                 </div>
             </div>
         </div>
