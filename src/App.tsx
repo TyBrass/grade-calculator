7a0397fd25d01e893116574982381653eb2e4635
Ty Brass <tybrass@outlook.com>
2024-04-23 20:41:50 -0500

    tsconfig change to prevent duplicate warnings; removed default asset; build most of the layout for this very simple app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [grades, setGrades] = useState<[number, number][]>([[0, 0], [0, 0], [0, 0], [0, 0]])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex flex-col items-center align-items h-screen max-w-[800px]'>
+        <h1>Grade Calculator</h1>
+        <p className='my-[10px] max-w-[600px]'>Input weighting and received grades for each piece of coursework to see what average is required on remaining coursework to achieve certain final marks in your class</p>
+        <div className='bg-slate-200 w-[400px]'>
+            <div className='flex justify-evenly'>
+                <h2>Weights</h2>
+                <h2>Grades</h2>
+            </div>
+            {grades.map(c => (
+                <>
+                <div className='flex justify-evenly mb-[8px]'>
+                    <input className='w-[50px]' type="number" min='0' max='100' placeholder='0' />
+                    <input className='w-[50px]' type="number" min='0' max='100' placeholder='0' />
+                </div>
+                </>
+            ))}
+            <div className='flex justify-evenly mb-[4px] border-t-2 border-white'>
+                <div>
+                    <h2 className='italic'>Weight Remaining</h2>
+                    {100 * grades.reduce((accumulator, currentValue) => accumulator + currentValue[0], 0)}
+                </div>
+                <div>
+                    <h2 className='italic'>Current Grade</h2>
+                    {grades.reduce((accumulator, currentValue) => accumulator + currentValue[0] * currentValue[1], 0) * grades.reduce((accumulator, currentValue) => accumulator + currentValue[0], 0)}
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
